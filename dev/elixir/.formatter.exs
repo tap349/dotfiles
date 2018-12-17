aabeb65ef876365b6d8659af2d0bf88e1eeabde4
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-12-17 14:20:31 +0300

    update formatter.exs

--- dev/elixir/.formatter.exs
+++ dev/elixir/.formatter.exs
@@ -1,17 +1,8 @@
 # https://hexdocs.pm/elixir/master/Code.html#format_string!/2-options
 [
   # these packages have adopted .formatter.exs
-  # => there is no need in locals_without_parens option now
-  import_deps: [:ecto, :phoenix, :plug],
-  inputs: ["{config,lib,test}/**/*.{ex,exs}", "mix.exs"],
+  import_deps: [:phoenix, :ecto, :plug],
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
   line_length: 80,
-  locals_without_parens: [
-    defenum: 2,
-    # added in Phoenix master but not in current release:
-    action_fallback: 1,
-    pipe_through: 1,
-    resources: 3,
-    socket: 2,
-    transport: 2
-  ]
+  locals_without_parens: [defenum: 2]
 ]
