96f5aee05bcd484f377192e450b8fcb60008603e
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-12-18 18:06:28 +0300

    update formatter.exs

--- dev/elixir/.formatter.exs
+++ dev/elixir/.formatter.exs
@@ -1,8 +1,5 @@
-# https://hexdocs.pm/elixir/master/Code.html#format_string!/2-options
 [
-  # these packages have adopted .formatter.exs
-  import_deps: [:phoenix, :ecto, :plug],
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  line_length: 80,
-  locals_without_parens: [defenum: 2]
+  import_deps: [:ecto, :phoenix],
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  subdirectories: ["priv/*/migrations"]
 ]
