a6e3d0b91079f234df7a9a6fad35cb6b2fb2b61f
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2019-09-01 15:10:56 +0300

    add ydl alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -50,6 +50,7 @@ alias mcu='mc -u'
 # (#i) - case-insensitive globbing
 # (Om) - sort by modification date (asc)
 alias q='open -Fn (#i)*.(jpeg|jpg|png)(Om)'
+alias ydl='youtube-dl'
 
 #-------------------------------------------------------------------------------
 # dev (general)
