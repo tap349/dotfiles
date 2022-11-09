bc5a88479b19e20ae531943324eb52ed70d98b9c
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2022-11-09 20:14:41 +0200

    Fix vertical line colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,7 +62,7 @@
 (load-theme 'aircon t)
 
 ;; https://emacs.stackexchange.com/a/69091
-(set-face-foreground 'vertical-border "#BABACA")
+(set-face-foreground 'vertical-border "#D8D8DE")
 
 ;; https://www.emacswiki.org/emacs/SetFonts
 (set-face-attribute 'default nil :font "Input-15")
@@ -401,4 +401,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fill-column-indicator ((t (:foreground "gray83")))))
+ '(fill-column-indicator ((t (:foreground "#DEE4EF")))))
