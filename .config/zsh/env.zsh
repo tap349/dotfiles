9e497ea632d8c8d7e1f017005f7cc1ab946b5f23
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2023-01-03 23:17:36 +0200

    Add ~/bin to PATH

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -43,6 +43,7 @@ path=(/usr/local/bin /opt/homebrew/bin $path)
 path=(/opt/homebrew/opt/libpq/bin $path)
 path=($HOME/soft/google-cloud-sdk/bin $path)
 path=(/Library/Java/JavaVirtualMachines/graalvm-ce-java19-22.3.0/Contents/Home/bin $path)
+path=($HOME/bin $path)
 
 #-------------------------------------------------------------------------------
 # Prompt
