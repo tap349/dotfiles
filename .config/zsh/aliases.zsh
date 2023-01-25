c5efe9826249f47f137b547ad8d860dd1e7bc926
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2023-01-25 17:14:18 +0200

    Rename aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -44,11 +44,11 @@ alias ydl='youtube-dl'
 alias cr='cockroach start-single-node --insecure --http-port=26256 --host=localhost'
 alias d='docker'
 alias k='kubectl'
-alias kcr-dpo='kubectl exec -it dev-platform-orchestrator-db-client -n platform -- cockroach sql --certs-dir=/cockroach/cockroach-certs --host=dev-platform-orchestrator-db-public --database=dpo'
-alias kl-dpo='kubectl logs -fl app.kubernetes.io/name=dev-platform-orchestrator -n platform | jq "{timestamp,level,message}"'
-alias kl-dpas='kubectl logs -fl app.kubernetes.io/name=dev-platform-access-service -n platform | jq "{timestamp,level,message}"'
-alias kpf-dpo='kubectl port-forward service/dev-platform-orchestrator-db-public -n platform 26257'
-alias kpf-dpas='kubectl port-forward service/dev-platform-access-service-db-public -n platform 26257'
+alias kcrdpo='kubectl exec -it dev-platform-orchestrator-db-client -n platform -- cockroach sql --certs-dir=/cockroach/cockroach-certs --host=dev-platform-orchestrator-db-public --database=dpo'
+alias kldpo='kubectl logs -fl app.kubernetes.io/name=dev-platform-orchestrator -n platform | jq "{timestamp,level,thread,message}"'
+alias kldpas='kubectl logs -fl app.kubernetes.io/name=dev-platform-access-service -n platform | jq "{timestamp,level,thread,message}"'
+alias kpfdpo='kubectl port-forward service/dev-platform-orchestrator-db-public -n platform 26257'
+alias kpfdpas='kubectl port-forward service/dev-platform-access-service-db-public -n platform 26257'
 
 #-------------------------------------------------------------------------------
 # Clojure
