f1904ebf1cc0ccde756c5928944edc59213a2678
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2023-03-22 16:48:37 +0200

    Make ivy-current-match bg color lighter

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -946,7 +946,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ivy-current-match ((t (:extend t :background "#eae6a2" :foreground "black"))))
+ '(ivy-current-match ((t (:extend t :background "#ece8a4" :foreground "black"))))
  '(ivy-minibuffer-match-face-1 ((t nil)))
  '(ivy-minibuffer-match-face-2 ((t (:inherit 'lazy-highlight))))
  '(ivy-minibuffer-match-face-3 ((t (:inherit 'lazy-highlight))))
