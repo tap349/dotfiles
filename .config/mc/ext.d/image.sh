f05239cbb7c260bf1b2e74aac8e2855573fa8ce7
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2023-04-12 01:09:31 +0300

    Misc

--- .config/mc/ext.d/image.sh
+++ .config/mc/ext.d/image.sh
@@ -39,6 +39,7 @@ do_open_action() {
         (inkscape "${MC_EXT_FILENAME}" &)
         ;;
     *)
+        # See mvi alias (zsh aliases are not visible here)
         (mpv --keep-open=yes "${MC_EXT_FILENAME}" >/dev/null 2>&1 &)
         ;;
     esac
