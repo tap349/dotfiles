f2ff4214865d0b368d322bb563c086d844f2fd01
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2023-04-16 20:48:04 +0300

    Customize doc.sh to open PDF in mc

---
+++ .config/mc/ext.d/doc.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# $1 - action
+# $2 - type of file
+
+action=$1
+filetype=$2
+
+[ -n "${MC_XDG_OPEN}" ] || MC_XDG_OPEN="xdg-open"
+
+do_view_action() {
+    filetype=$1
+
+    case "${filetype}" in
+    *)
+        mcedit "${MC_EXT_FILENAME}"
+        ;;
+    esac
+}
+
+do_open_action() {
+    filetype=$1
+
+    case "${filetype}" in
+    *)
+        open "${MC_EXT_FILENAME}"
+        ;;
+    esac
+}
+
+case "${action}" in
+view)
+    do_view_action "${filetype}"
+    ;;
+open)
+    ("${MC_XDG_OPEN}" "${MC_EXT_FILENAME}" >/dev/null 2>&1) || \
+        do_open_action "${filetype}"
+    ;;
+*)
+    ;;
+esac
