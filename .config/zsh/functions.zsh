c555d18d95fcefe180fd94d59ba13a824c0df9ed
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2023-07-13 15:17:28 +0300

    Add backup db to kpf

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -164,6 +164,7 @@ kpf() {
   kubectl port-forward service/user-service-db-public -n platform 26263:26257 &
   kubectl port-forward service/pipeline-db-public -n platform 26264:26257 &
   kubectl port-forward service/template-db-public -n platform 26265:26257 &
+  kubectl port-forward service/backup-db-public -n platform 26266:26257 &
 
   # Run function in foreground
   wait
