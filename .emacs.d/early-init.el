4d6d47d10fea97b6eb42b76986d78c26548d4eb7
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2023-11-06 15:19:24 +0200

    Add BLAH-m key

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,3 +2,11 @@
 
 ;; Use straight.el instead of package.el
 (setq package-enable-at-startup nil)
+
+;; https://emacsnotes.wordpress.com/2022/09/11/three-bonus-keys-c-i-c-m-and-c-for-your-gui-emacs-all-with-zero-headache/
+(add-hook
+ 'after-make-frame-functions
+ (defun setup-blah-keys (frame)
+   (with-selected-frame frame
+     (when (display-graphic-p)
+       (define-key input-decode-map (kbd "C-m") [BLAH-m])))))
