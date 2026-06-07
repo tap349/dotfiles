db1ccac3a52ac395c6004977176d587ae966f8dc
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2026-06-07 17:27:57 +0300

    Fix zsh prompt to use line concatenation

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -2,46 +2,37 @@
 setopt prompt_subst
 
 # for i in {0..255}; do print -P "%F{$i}$i%f "; done
-PS1='\
-%K{#1F2F40}\
-%F{032} %1d %f\
-$(git_prompt)\
-%k\
-%F{032} %(!.#.») %f'
+PS1='%K{#1F2F40}%F{032} %1d %f$(git_prompt)%k%F{032} %(!.#.») %f'
 
 git_prompt() {
   if [[ ! -d .git ]]; then
     return 0
   fi
 
-  local branch=$(git symbolic-ref --short HEAD 2> /dev/null)
+  local branch
+  branch=$(git symbolic-ref --short HEAD 2> /dev/null) || return 0
 
   if [[ -z $branch ]]; then
     return 0
   fi
 
-  echo -n %F{75}
-  echo -n '('
-  echo -n $branch
-  echo -n %f
+  local output="%F{75}(${branch}%f"
 
-  local has_changes=$(git status --short)
+  local has_changes
+  has_changes=$(git status --short)
 
   if [[ -n $has_changes ]]; then
-    echo -n %F{214}
-    echo -n '*'
-    echo -n %f
+    output+="%F{214}*%f"
   fi
 
-  local last_tag=$(git tag --sort=-v:refname | head -n 1 2> /dev/null)
+  local last_tag
+  last_tag=$(git tag --sort=-v:refname | head -n 1 2> /dev/null)
 
   if [[ -n $last_tag ]]; then
-    echo -n %F{230}
-    echo -n " $last_tag"
-    echo -n %f
+    output+="%F{230} ${last_tag}%f"
   fi
 
-  echo -n %F{75}
-  echo -n ') '
-  echo -n %f
+  output+="%F{75}) %f"
+
+  printf '%s' "$output"
 }
