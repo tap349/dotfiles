baab790dbebeeb45166ea19ea72e0eb4fbdbfd20
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2026-09-09 00:45:31 +0300

    Rename eglot-find-typeDefinition to eglot-find-type-definition

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -665,13 +665,13 @@
         ("s-l g g" . xref-find-definitions)
         ("s-l g i" . eglot-find-implementation)
         ("s-l g r" . xref-find-references)
-        ("s-l g t" . eglot-find-typeDefinition)
+        ("s-l g t" . eglot-find-type-definition)
         ("s-l r o" . eglot-code-action-organize-imports)
         ("s-l r r" . eglot-rename)
         ;; Default keybindings in IntelliJ IDEA
         ("M-<return>" . eglot-code-action-quickfix)
         ("s-b" . xref-find-references)
-        ("s-B" . eglot-find-typeDefinition)
+        ("s-B" . eglot-find-type-definition)
         ("s-M-b" . eglot-find-implementation)))
 
 (use-package emacs
