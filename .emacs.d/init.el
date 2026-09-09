59e007830678ae2ba0d9d7817c7cb89763e0c3c2
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2026-09-10 01:02:31 +0300

    Use C-s for consult-line again

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -500,7 +500,7 @@
    ("M-s l" . consult-line)
    ("M-s r" . consult-ripgrep)
    ;; Use single-chord keys for fast access
-   ("s-f" . consult-line)
+   ("C-s" . consult-line)
    ("s-n" . consult-fd)
    ("s-r" . consult-ripgrep)))
 
